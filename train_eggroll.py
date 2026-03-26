42bd6e09be9890fd09ce741ae8a0e9479682b42f
Johann Zahlmann <johann.zahlmann@gmail.com>
2026-03-26 11:25:21 +0100

    2 BP warmup + 8 EGGROLL = 10 total epochs

--- train_eggroll.py
+++ train_eggroll.py
@@ -171,7 +171,7 @@ def train(seed=42):
     step = jnp.int32(0)
 
     # Backprop warmup epoch (gives EGGROLL a better starting point)
-    BACKPROP_WARMUP = 1
+    BACKPROP_WARMUP = 2
     BP_LR = 3e-3
 
     @jax.jit
